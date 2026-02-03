520fdae6c2ea4e1282ad412e27b869ebbfdd79b3
Bennah <bennah955@gmail.com>
2026-02-03 13:43:06 +0300

    selected all the needed elements

---
+++ profile.js
@@ -0,0 +1,19 @@
+// header selection
+let head = document.getElementById("header");
+
+// input field selecting
+let nameField = document.getElementById("nameInput");
+let techField = document.getElementById("techInput");
+let goalField = document.getElementById("goalInput");
+
+// button selection
+let btnUpdate = document.getElementById("updateBtn");
+let btnTheme = document.getElementById("themeBtn");
+let btnReset = document.getElementById("resetBtn");
+
+// card and its elements selection
+let cardBody = document.getElementById("card");
+let nameCard = document.getElementById("cardName");
+let techCard = document.getElementById("cardTech");
+let goalCard = document.getElementById("cardGoal");
+
